fa3f12c4808c0db661ae07b9c872b3cb250318bc
KHADIJAH WURAOLA AMUSAT <wuraola81@gmail.com>
2024-08-20 11:53:28 +0300

    aqara-ui

---
+++ components/How-it-works.js
@@ -0,0 +1,83 @@
+import { Heading, Text } from '@chakra-ui/react'
+import {
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Grid,
+  GridItem,
+} from '@chakra-ui/react'
+import { authorData, readerData } from '../utils/authordata'
+import HowItWorksCard from './How-it-works-card'
+
+const tabNames = ['Authors', 'Readers']
+
+const HowItWorks = () => {
+  return (
+    <div className="how-it-works">
+      <Heading
+        fontFamily="'Clash Grotesk', sans-serif"
+        fontSize="56px"
+        fontWeight="700"
+        mb={5}
+        mt={30}
+        textAlign="center"
+      >
+        Hedera Services used
+      </Heading>
+      <Tabs
+        variant="soft-rounded"
+        defaultIndex={0}
+        paddingTop={'50px'}
+        paddingBottom={'90px'}
+      >
+        <TabList
+          width="fit-content"
+          mx="auto"
+          background="#F3F4F6"
+          borderRadius="66px"
+          marginBottom={'40px'}
+        >
+          {tabNames.map((tab, index) => (
+            <Tab
+              key={index}
+              color="#000000"
+              fontSize="0px"
+              fontWeight="500"
+              _selected={{
+                color: '#FFC2A1',
+                background: '#281C36',
+                boxShadow: 'none',
+              }}
+            >
+              {tab}
+            </Tab>
+          ))}
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <Grid templateColumns={{base:"repeat(1, 1fr)", md:"repeat(2, 1fr)", lg:"repeat(4, 1fr)"}} gap={5}>
+              {authorData.map(({ id, step,content }) => (
+                <GridItem key={id} w="100%">
+                  <HowItWorksCard stepNumber={step} stepText={content}/>
+                </GridItem>
+              ))}
+            </Grid>
+          </TabPanel>
+          <TabPanel>
+            <Grid templateColumns={{base:"repeat(1, 1fr)", md:"repeat(2, 1fr)", lg:"repeat(4, 1fr)"}} gap={5}>
+              {readerData.map(({ id, step, content }) => (
+                <GridItem key={id} w="100%">
+                  <HowItWorksCard stepNumber={step} stepText={content} />
+                </GridItem>
+              ))}
+            </Grid>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </div>
+  )
+}
+
+export default HowItWorks
---
+++ utils/authorData.js
@@ -0,0 +1,42 @@
+export const authorData = [
+    {
+      id: 1,
+      step: 'Hedera Smart Contract',
+      content:
+        'Power claims processing, token issuance, and gamification with Hedera Smart Contract Service.',
+    },
+    {
+      id: 2,
+      step: 'Hedera Consensus Service',
+      content:
+        'Ensure transparency and integrity of transactions with fast, fair, and secure consensus.',
+    },
+    {
+      id: 3,
+      step: 'Hedera Token Service',
+      content:
+        'Manage and tokenize user contributions for fractional ownership and liquidit',
+    },
+    {
+      id: 4,
+      step: 'Hedera Mirror Nodes',
+      content:
+        'Monitor and analyze real-time data for user activity and platform performance.',
+    },
+  ]
+  export const readerData = [
+    {
+      id: 5,
+      step: 'Hedera File service',
+      content:
+        'Store and manage large files in a decentralized manner, ensuring data integrity and accessibility..',
+    }
+    
+  ]
+
+
+
+
+
+  
+  
